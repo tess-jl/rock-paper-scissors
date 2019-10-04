86881bc755c9ca8b717df9f108c16a9c96e6da1c
Tess Lameyer <tjlameyer@gmail.com>
2019-10-04 11:54:25 -0700

    refractoring and work in progress with the reset button that I will continue to work on this weekend

--- check-result.js
+++ check-result.js
@@ -3,23 +3,13 @@
 //returns string of either draw (throws are identical), win (player wins), or lose (player loses)
 
 const checkResult = (player, game) => {
-    if (player === 'rock' && game === 'rock') {
+    if (player === game) {
         return 'draw';
-    } else if (player === 'paper' && game === 'paper') {
-        return 'draw';
-    } else if (player === 'scissors' && game === 'scissors') {
-        return 'draw';
-    } else if (player === 'rock' && game === 'paper') {
+
+    } else if (player === 'rock' && game === 'paper' || player === 'paper' && game === 'scissors' || player === 'scissors' && game === 'rock') {
         return 'game wins';
-    } else if (player === 'paper' && game === 'scissors') {
-        return 'game wins';
-    } else if (player === 'scissors' && game === 'rock') {
-        return 'game wins';
-    } else if (player === 'rock' && game === 'scissors') {
-        return 'player wins';
-    } else if (player === 'paper' && game === 'rock') {
-        return 'player wins';
-    } else if (player === 'scissors' && game === 'paper') {
+
+    } else {
         return 'player wins';
     }
 };
--- r-p-s.js
+++ r-p-s.js
@@ -2,52 +2,58 @@
 import checkResult from './check-result.js';
 import getRandomThrow from './get-random-throw.js';
 
-// reference the DOM elements
-const playButton = document.getElementById('play-button'); // button user presses
-const throwResult = document.getElementById('throw-result'); // message to update user about changed state
-// DOM elements for tracking results for user
+// initialize/fetch DOM elements
+const playButton = document.getElementById('play-button'); 
+// const resetButton = document.getElementById('reset-button'); 
+const throwResult = document.getElementById('throw-result'); 
 const playerThrow = document.getElementById('player-throw');
 const gameThrow = document.getElementById('game-throw');
-
 const numberOfDraws = document.getElementById('draws');
 const numberOfGameWins = document.getElementById('game-wins');
 const numberOfPlayerWins = document.getElementById('player-wins');
 
 
-// set up application state (will change over time)
+// set some initial state
 let draw = 0;
 let gameWins = 0;
 let playerWins = 0;
-
 numberOfDraws.textContent = draw;
 numberOfGameWins.textContent = gameWins;
 numberOfPlayerWins.textContent = playerWins;
 
-// Event-handler function 
-playButton.addEventListener('click', () => {
+
+
+// DOM utility functions
+const playGame = () => {
+
     const randomGameThrow = getRandomThrow();
     const playerGameThrow = document.querySelector('input:checked').value;
-
     playerThrow.textContent = playerGameThrow;
     gameThrow.textContent = randomGameThrow;
 
     const result = checkResult(playerGameThrow, randomGameThrow);
-    console.log(randomGameThrow, 'random game throw');
-    console.log(playerGameThrow, 'player game throw');
-
-
 
     if (result === 'draw') {
-        throwResult.textContent = 'it was a draw!';
-        draw = draw + 1;
+        throwResult.textContent = `it's a draw!`;
+        draw++;
         numberOfDraws.textContent = draw;
     } else if (result === 'game wins') {
-        throwResult.textContent = 'you lost!';
-        gameWins = gameWins + 1;
+        throwResult.textContent = `you lost!`;
+        gameWins++;
         numberOfGameWins.textContent = gameWins;
-    } else if (result === 'player wins') {
-        throwResult.textContent = 'you won!';
-        playerWins = playerWins + 1;
+    } else {
+        throwResult.textContent = `you won!`;
+        playerWins++;
         numberOfPlayerWins.textContent = playerWins;
     }
-});
+};
+
+// const hardRest = () => {
+
+// };
+
+
+
+// add event listeners
+// resetButton.addEventListener('click', hardRest);
+playButton.addEventListener('click', playGame);
